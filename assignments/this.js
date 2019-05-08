220d225aa43d93b50d8f0f6362d447c0e4d20e17
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-08 14:34:23 +0100

    Explainthe four principles of 'this' in this.js file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. In the global scope, the this keyword refers to the windows object and it houses all variables and functions defined in the global scope
+* 2. When a function is called using the dot notation (e.g obj.test()), the this keyword in that function will refer to the object before the dot notation
+* 3. Every time an instance of a class is created, the constructor runs and the this keyword inside that constructor refers to that instance of that class at that time 
+* 4. When calling a method using javascripts call() or apply() method, the this keyword is explicitly defined as first arguement.
 *
 * write out a code example of each explanation above
 */
